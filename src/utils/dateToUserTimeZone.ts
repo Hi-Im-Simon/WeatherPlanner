1e7a89a6af87c16052f1ac9932f60cd6a4989685
Szymon Stanisławski <szymol1001@gmail.com>
2023-05-31 01:56:09 +0200

    fix (?) calculating user fixed date

--- src/utils/dateToUserTimeZone.ts
+++ src/utils/dateToUserTimeZone.ts
@@ -1,7 +1,9 @@
 import { DateTime } from 'luxon';
 
 const dateToUserTimeZone = (dateString: string): DateTime => {
-    const convertedDate = DateTime.fromISO(`${dateString}`, { zone: 'GMT' });
+    const tzOff = (new Date()).getTimezoneOffset() / 60;
+    const tzOffsetString = `${tzOff < 0 ? '-' : '+'}${Math.abs(tzOff) < 10 ? '0' : ''}${Math.abs(tzOff)}:00`;
+    const convertedDate = DateTime.fromISO(`${dateString}${tzOffsetString}`, { zone: 'GMT' });
     return convertedDate;
 }
 
